b9152138ba7302df46b90aef95c3fd033e79d8bc
github username <github email address>
2019-11-23 16:02:28 -0700

    Done.

--- index.js
+++ index.js
@@ -4,19 +4,42 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    // Iterate over collection, pass ea el to callback, return original collection
+    // Ea invocation of callback has args el, i, collection (el is value for obj)
+    each: function(collection, callback) {
+      // Turn collection into an arr, if obj it's an arr of values
+      let array = (typeof(collection) === 'object') ? Object.values(collection) : collection.slice()
+      // For ea el of array, fire callback w that el
+      for (const el of array) {callback(el)}
+      // Return original collection
+      return collection
     },
 
-    map: function() {
-
+    // Iterate over collection, pass ea el to callback, return new arr w new values (w/o modifying original collection)
+    // Ea invocation of callback has args el, i, collection (el is value for obj)
+    map: function(collection, callback) {
+      // Turn collection into an arr, if obj it's an arr of values
+      let array = (typeof(collection) === 'object') ? Object.values(collection) : collection.slice()
+      // Initiate results arr for new values
+      let results = []
+      // For ea el of array, fire callback w that el and push into results
+      for (let i = 0; i < array.length; i++) {
+        results.push(callback(array[i]))
+      }
+      // Return new arr w new values (results)
+      return results
     },
 
-    reduce: function() {
-
+    // Aggregate collection into a single value
+    // Acc is the initial value that ea result of callback should add to
+    // Ea invocation of callback has args acc, current value, and collection
+    reduce: function(collection, callback, acc) {
+      // Turn collection into an arr, if obj it's an arr of values
+      let array = (typeof(collection) === 'object') ? Object.values(collection) : collection.slice()
+      
     },
 
-    functions: function() {
+    functions: function(collection, predicate) {
 
     },
 
